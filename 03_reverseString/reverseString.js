22b7dc25a7846934cae2c439687e02d56ca136fe
nathan <codejourney2022@gmail.com>
2022-04-08 17:36:27 -0400

    Add change to reverString()
    added the variable input and a for loop statement that iterates over the length of the string argument, and returns the last letter of the string into the input variable every loop. This will create a reverse string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let input = ' ';
+    for (let i = 0; i < input.length; i++) {
+        return input += string.slice(-1);
+    }
 };
 
 // Do not edit below this line

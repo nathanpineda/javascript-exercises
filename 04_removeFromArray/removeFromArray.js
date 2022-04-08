38f6d2cfd818667389e14f3d1a7dda4c8046c94c
nathan <codejourney2022@gmail.com>
2022-04-08 19:41:47 -0400

    Make change to removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num = 0, removeElement = 1) {
+    const list = array;
+    list.splice(list[num], removeElement);
+    return list;
 };
 
 // Do not edit below this line

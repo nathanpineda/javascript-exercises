6e1b01987f1693437e859b470aaa791b6f9a6eed
nathan <codejourney2022@gmail.com>
2022-04-09 21:40:45 -0400

    Change if statment within loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,10 +2,10 @@ const removeFromArray = function(array, ...args) {
     const list = array;
     const argArray = args;
     
-    for (let i = 0; i < argArray.length; i++) {
-        if (argArray[i]) {
-            list.slice(list.indexOf(argArray[i]));
-        }
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === argArray[0]) {
+            list.slice(i);
+        } 
     }
 
     return list;

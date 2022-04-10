5f06856b61833f9c8f830912f4d72163893e7dd2
nathan <codejourney2022@gmail.com>
2022-04-10 13:51:27 -0400

    Revamp removeFromArray and implement a filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,8 @@
 const removeFromArray = function(array, ...args) {
     const list = array;
     const argArray = args;
-    const combinedListArgs = [array, args];
-    const newList = [];
     
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] !== argArray[0]) {
-            newList.push(list[i]);
-        }
-    }
-    
-    console.log(combinedListArgs);
-
-    return newList;
+    return list.filter(elements => !argArray.includes(elements));
 };
 
 // Do not edit below this line

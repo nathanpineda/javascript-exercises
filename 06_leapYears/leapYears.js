6d3522c173205b907fc310290e37713c17927e68
nathan <codejourney2022@gmail.com>
2022-04-10 21:02:41 -0400

    Create if else statements within leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
 const leapYears = function(year) {
     let inputYear = year;
-    if ()
+    let isLeapYear;
+    let divisibleBy100 = 0;
+
+    if(inputYear % 100 === 0) {
+        divisibleBy100 += inputYear;
+    }
+
+    if (inputYear % 4 !== 0) {
+        isLeapYear = false;
+    } else if (inputYear % 100 === 0 && inputYear % 400 !== 0) {
+        isLeapYear = false;
+    } else if (inputYear % 100 === 0 && inputYear % 400 === 0) {
+        isLeapYear = true;
+    } else {
+        isLeapYear = true;
+    }
 };
 
 // Do not edit below this line
